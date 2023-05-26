7029fe2a0b737379824f538b39306dd474bc1743
Pranav Pokale <106576132+pranav50034@users.noreply.github.com>
2023-05-26 15:03:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const prices = document.querySelectorAll('td.kimmat')
+let sum = 0
+prices.forEach((e)=>{
+	sum+=Number(e.innerText)
+})
+const tr = document.createElement('tr')
+tr.innerHTML = `<td data-ns-test="grandTotal">Total: ${sum}</td>`
+document.querySelector('.tbody').appendChild(tr)
